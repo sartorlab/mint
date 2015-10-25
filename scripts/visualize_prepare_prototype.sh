310da6f2fd4aa3c368a55b4739781c59814ddeae
Raymond Cavalcante <rcavalca@umich.edu>
2015-10-25 13:04:18 -0400

    0.7.5.6 Plot titles and fixed name collisions
    * Added plot titles
    * Added _annot_ to annotation files to prevent pattern matching from
    picking up incorrect files in the visualize_prototype.R script.

--- scripts/visualize_prepare_prototype.sh
+++ scripts/visualize_prepare_prototype.sh
@@ -1,42 +1,51 @@
 cd ~/latte/mint/IDH2mut_v_NBM
 
 ################################################################################
+# Average % methylation and coverage in 5kb tiling regions
+
+  # % methylation
+  bedtools intersect -a ../data/annotation/windows_5kb_hg19.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_meth_5kb_windows.bed
+
+  # Coverage
+  bedtools intersect -a ../data/annotation/windows_5kb_hg19.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $8 + $9}' | bedtools groupby -g 1-3 -c 4 -o mean > IDH2mut_1_mc_hmc_avg_coverage_5kb_windows.bed
+
+################################################################################
 # Average % methylation in annotated regions
 
   # Find average % methylation in CpG islands
-  bedtools intersect -a ../data/annotation/cpg_islands_hg19_ucsc.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_meth_cpg_islands.bed
+  bedtools intersect -a ../data/annotation/cpg_islands_hg19_ucsc.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_meth_annot_cpg_islands.bed
 
   # Find average % methylation in CpG shores
-  bedtools intersect -a ../data/annotation/cpg_shores_hg19_ucsc.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_meth_cpg_shores.bed
+  bedtools intersect -a ../data/annotation/cpg_shores_hg19_ucsc.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_meth_annot_cpg_shores.bed
 
   # Find average % methylation in promoters
   # On unique chrom, start, end, geneid because we want to preserve all of that information
   # For promoters, swap the geneid and quantity we're interested in columns so
   # that read-in to R consistently has the quantity in the 4th column
-  bedtools intersect -a ../data/annotation/ldef_5kb_hg19_reduced.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | bedtools groupby -g 1-4 -c 8 -o mean | awk -v OFS='\t' '{print $1, $2, $3, $5, $4}' > IDH2mut_1_mc_hmc_avg_meth_promoters.bed
+  bedtools intersect -a ../data/annotation/ldef_5kb_hg19_reduced.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | bedtools groupby -g 1-4 -c 8 -o mean | awk -v OFS='\t' '{print $1, $2, $3, $5, $4}' > IDH2mut_1_mc_hmc_avg_meth_annot_promoters.bed
 
   # Find average % methylation in enhancers
   # Use the Andersson enhancers to start
-  bedtools intersect -a ../data/annotation/andersson_permissive_enhancers.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $13, $14, $15, $16}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_meth_andersson_permissive_enhancers.bed
+  bedtools intersect -a ../data/annotation/andersson_permissive_enhancers.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $13, $14, $15, $16}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_meth_annot_andersson_permissive_enhancers.bed
 
 ################################################################################
 # Average bisulfite coverage in annotated regions
 
   # Find average bisulfite coverage in CpG islands
-  bedtools intersect -a ../data/annotation/cpg_islands_hg19_ucsc.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6, $8 + $9}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_coverage_cpg_islands.bed
+  bedtools intersect -a ../data/annotation/cpg_islands_hg19_ucsc.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6, $8 + $9}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_coverage_annot_cpg_islands.bed
 
   # Find average bisulfite coverage in CpG shores
-  bedtools intersect -a ../data/annotation/cpg_shores_hg19_ucsc.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6, $8 + $9}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_coverage_cpg_shores.bed
+  bedtools intersect -a ../data/annotation/cpg_shores_hg19_ucsc.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6, $8 + $9}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_coverage_annot_cpg_shores.bed
 
   # Find average bisulfite coverage in promoters
   # On unique chrom, start, end, geneid because we want to preserve all of that information
   # For promoters, swap the geneid and quantity we're interested in columns so
   # that read-in to R consistently has the quantity in the 4th column
-  bedtools intersect -a ../data/annotation/ldef_5kb_hg19_reduced.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6, $7, $9 + $10}' | bedtools groupby -g 1-4 -c 8 -o mean | awk -v OFS='\t' '{print $1, $2, $3, $5, $4}' > IDH2mut_1_mc_hmc_avg_coverage_promoters.bed
+  bedtools intersect -a ../data/annotation/ldef_5kb_hg19_reduced.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6, $7, $9 + $10}' | bedtools groupby -g 1-4 -c 8 -o mean | awk -v OFS='\t' '{print $1, $2, $3, $5, $4}' > IDH2mut_1_mc_hmc_avg_coverage_annot_promoters.bed
 
   # Find average bisulfite coverage in enhancers
   # Use the Andersson enhancers to start
-  bedtools intersect -a ../data/annotation/andersson_permissive_enhancers.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $13, $14, $15, $17 + $18}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_coverage_andersson_permissive_enhancers.bed
+  bedtools intersect -a ../data/annotation/andersson_permissive_enhancers.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $13, $14, $15, $17 + $18}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_coverage_annot_andersson_permissive_enhancers.bed
 
 ################################################################################
 # Average change in methylation in DMRs in annotated regions
@@ -48,13 +57,13 @@ cd ~/latte/mint/IDH2mut_v_NBM
   # $17 = pvalue, $18 = qvalue, $19 = meth diff, $23 = meth in group 1, $24 = meth in group 0 (intersection + enhancers)
 
   # Find average % methylation change in CpG islands
-  bedtools intersect -a ../data/annotation/cpg_islands_hg19_ucsc.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 10 -o mean > IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_cpg_islands.bed
+  bedtools intersect -a ../data/annotation/cpg_islands_hg19_ucsc.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 10 -o mean > IDH2mut_v_NBM_mc_hmc_avg_diff_meth_in_DM_annot_cpg_islands.bed
 
   # Find average % methylation change in CpG islands
-  bedtools intersect -a ../data/annotation/cpg_shores_hg19_ucsc.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 10 -o mean > IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_cpg_shores.bed
+  bedtools intersect -a ../data/annotation/cpg_shores_hg19_ucsc.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 10 -o mean > IDH2mut_v_NBM_mc_hmc_avg_diff_meth_in_DM_annot_cpg_shores.bed
 
   # Find average % methylation change in promoters
-  bedtools intersect -a ../data/annotation/ldef_5kb_hg19_reduced.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-4 -c 11 -o mean | awk -v OFS='\t' '{print $1, $2, $3, $5, $4}' > IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_promoters.bed
+  bedtools intersect -a ../data/annotation/ldef_5kb_hg19_reduced.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-4 -c 11 -o mean | awk -v OFS='\t' '{print $1, $2, $3, $5, $4}' > IDH2mut_v_NBM_mc_hmc_avg_diff_meth_in_DM_annot_promoters.bed
 
   # Find average % methylation change in enhancers
-  bedtools intersect -a ../data/annotation/andersson_permissive_enhancers.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 19 -o mean > IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_andersson_permissive_enhanceres.bed
+  bedtools intersect -a ../data/annotation/andersson_permissive_enhancers.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 19 -o mean > IDH2mut_v_NBM_mc_hmc_avg_diff_meth_in_DM_annot_andersson_permissive_enhanceres.bed
--- scripts/visualize_prototype.R
+++ scripts/visualize_prototype.R
@@ -9,19 +9,32 @@ setwd('~/Desktop')
   data$coverage = log10(data$numC + data$numT)
 
   # % methylation
-  per_meth_plot = ggplot(data, aes(perc_meth)) + geom_histogram(binwidth=5, aes(y=..density..)) + theme_bw()
+  per_meth_plot = ggplot(data, aes(perc_meth)) + geom_histogram(binwidth=5, aes(y=..density..)) + ggtitle('% Methylation Across All Sites') + theme_bw()
   ggsave(filename='IDH2mut_1_mc_hmc_sample_per_meth.png', plot = per_meth_plot, width=6, height=6, dpi=300)
 
   # coverage
-  coverage_plot = ggplot(data, aes(coverage)) + geom_histogram(aes(y=..density..)) + theme_bw()
+  coverage_plot = ggplot(data, aes(coverage)) + geom_histogram(aes(y=..density..)) + ggtitle('Coverage Across All Sites') + theme_bw()
   ggsave(filename='IDH2mut_1_mc_hmc_sample_coverage.png', plot = coverage_plot, width=6, height=6, dpi=300)
+
+################################################################################
+# Average % methylation and coverage across 5kb tiles of hg19
+
+  data_pm = read.table('IDH2mut_1_mc_hmc_avg_meth_5kb_windows.bed', header=F, sep='\t', quote='', comment.char='', stringsAsFactors=F, col.names=c('chrom','start','end','avg_meth'))
+
+  data_cov = read.table('IDH2mut_1_mc_hmc_avg_coverage_5kb_windows.bed', header=F, sep='\t', quote='', comment.char='', stringsAsFactors=F, col.names=c('chrom','start','end','avg_coverage'))
+
+  avg_meth_plot = ggplot(data_pm, aes(avg_meth)) + geom_histogram(binwidth=5, aes(y=..density..)) + ggtitle('Average % Methylation Across 5kb Windows') + theme_bw()
+  avg_cov_plot = ggplot(data_cov, aes(log10(avg_coverage))) + geom_histogram(aes(y=..density..)) + ggtitle('Average Coverage Across 5kb Windows') + theme_bw()
+
+  ggsave(filename='IDH2mut_1_mc_hmc_avg_meth_5kb_windows.png', plot = avg_meth_plot, width = 6, height = 6, dpi = 300)
+  ggsave(filename='IDH2mut_1_mc_hmc_avg_coverage_5kb_windows.png', plot = avg_cov_plot, width = 6, height = 6, dpi = 300)
 
 ################################################################################
 # Average % methylation across annotations (bismark.cov.gz)
 
-  files = list.files(pattern='avg_meth')
+  files = list.files(pattern='IDH2mut_1_mc_hmc_avg_meth_annot_')
 
-  annotations = gsub('IDH2mut_1_mc_hmc_avg_meth_','',files)
+  annotations = gsub('IDH2mut_1_mc_hmc_avg_meth_annot_','',files)
   annotations = gsub('.bed','',annotations)
 
   data = lapply(files, read.table, sep='\t', header=F, quote='', comment.char='', stringsAsFactors=F)
@@ -29,16 +42,16 @@ setwd('~/Desktop')
 
   df = Reduce(rbind, lapply(names(data), function(n){data.frame('annotation'=n,'avg_meth'=data[[n]][,4],stringsAsFactors=F)}))
 
-  avg_meth_plot = ggplot(df, aes(avg_meth)) + geom_histogram(binwidth=5, aes(y=..density..)) + facet_grid(. ~ annotation) + theme_bw()
+  avg_meth_plot = ggplot(df, aes(avg_meth)) + geom_histogram(binwidth=5, aes(y=..density..)) + facet_grid(. ~ annotation) + ggtitle('Average % Methylation Across Annotations') + theme_bw()
 
   ggsave(filename='IDH2mut_1_mc_hmc_avg_meth_annotations.png', plot = avg_meth_plot, width = 8, height = 4, dpi = 300)
 
 ################################################################################
 # Average coverage across annotations (bismark.cov.gz)
 
-  files = list.files(pattern='avg_coverage')
+  files = list.files(pattern='IDH2mut_1_mc_hmc_avg_coverage_annot_')
 
-  annotations = gsub('IDH2mut_1_mc_hmc_avg_coverage_','',files)
+  annotations = gsub('IDH2mut_1_mc_hmc_avg_coverage_annot_','',files)
   annotations = gsub('.bed','',annotations)
 
   data = lapply(files, read.table, sep='\t', header=F, quote='', comment.char='', stringsAsFactors=F)
@@ -46,9 +59,9 @@ setwd('~/Desktop')
 
   df = Reduce(rbind, lapply(names(data), function(n){data.frame('annotation'=n,'avg_coverage'=log10(data[[n]][,4]),stringsAsFactors=F)}))
 
-  avg_meth_plot = ggplot(df, aes(avg_coverage)) + geom_histogram(aes(y=..density..)) + facet_grid(. ~ annotation) + theme_bw()
+  avg_cov_plot = ggplot(df, aes(avg_coverage)) + geom_histogram(aes(y=..density..)) + facet_grid(. ~ annotation) + ggtitle('Average Coverage Across Annotations') + theme_bw()
 
-  ggsave(filename='IDH2mut_1_mc_hmc_avg_coverage_annotations.png', plot = avg_meth_plot, width = 8, height = 4, dpi = 300)
+  ggsave(filename='IDH2mut_1_mc_hmc_avg_coverage_annotations.png', plot = avg_cov_plot, width = 8, height = 4, dpi = 300)
 
 ################################################################################
 # Average % methylation change across all DMRs (methylSig)
@@ -56,27 +69,27 @@ setwd('~/Desktop')
   data = read.table('IDH2mut_v_NBM.txt', header=T, sep='\t', quote='', comment.char='', stringsAsFactors=F)
   sig = subset(data, pvalue < 0.05)
 
-  avg_DM = data.frame('annotation'='all_tested', 'meth_diff'=data$meth.diff, stringsAsFactors=F)
-  avg_DM_sig = data.frame('annotation'='significantly_DM', 'meth_diff'=sig$meth.diff, stringsAsFactors=F)
+  DM = data.frame('annotation'='all_tested', 'meth_diff'=data$meth.diff, stringsAsFactors=F)
+  DM_sig = data.frame('annotation'='significantly_DM', 'meth_diff'=sig$meth.diff, stringsAsFactors=F)
 
-  region_DM = data.frame('annotation'='all_tested', 'meth1'=data$mu1, 'meth0'=data$mu0, stringsAsFactors=F)
-  region_DM_sig = data.frame('annotation'='significantly_DM', 'meth1'=sig$mu1, 'meth0'=sig$mu0, stringsAsFactors=F)
+  DM_groups = data.frame('annotation'='all_tested', 'meth1'=data$mu1, 'meth0'=data$mu0, stringsAsFactors=F)
+  DM_groups_sig = data.frame('annotation'='significantly_DM', 'meth1'=sig$mu1, 'meth0'=sig$mu0, stringsAsFactors=F)
 
-  df = rbind(avg_DM, avg_DM_sig)
-  region_df = rbind(region_DM, region_DM_sig)
+  df = rbind(DM, DM_sig)
+  df_groups = rbind(DM_groups, DM_groups_sig)
 
-  diff_meth_plot = ggplot(df, aes(meth_diff)) + geom_histogram(binwidth=5, aes(y=..density..)) + facet_grid(. ~ annotation) + theme_bw()
-  point_plot = ggplot(region_df, aes(meth1, meth0)) + geom_bin2d(binwidth=c(1,1)) + facet_grid(. ~ annotation)
+  diff_meth_plot = ggplot(df, aes(meth_diff)) + geom_histogram(binwidth=5, aes(y=..density..)) + facet_grid(. ~ annotation) + ggtitle('% Methylation Difference Between Groups') + theme_bw()
+  diff_meth_groups_plot = ggplot(df_groups, aes(meth1, meth0)) + geom_bin2d(binwidth=c(1,1)) + facet_grid(. ~ annotation) + ggtitle('% Methylation of Groups (DMR-Matched)')
 
   ggsave(filename='IDH2mut_v_NBM_mc_hmc_diff_meth.png', plot = diff_meth_plot, width = 8, height = 4, dpi = 300)
-  ggsave(filename='IDH2mut_v_NBM_mc_hmc_diff_meth_point_heatmap.png', plot = point_plot, width = 8, height = 4, dpi = 300)
+  ggsave(filename='IDH2mut_v_NBM_mc_hmc_diff_meth_point_heatmap.png', plot = diff_meth_groups_plot, width = 8, height = 4, dpi = 300)
 
 ################################################################################
 # Average % methylation change across annotations (methylSig)
 
-  files = list.files(pattern='avg_meth_chg')
+  files = list.files(pattern='IDH2mut_v_NBM_mc_hmc_avg_diff_meth_in_DM_annot_')
 
-  annotations = gsub('IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_','',files)
+  annotations = gsub('IDH2mut_v_NBM_mc_hmc_avg_diff_meth_in_DM_annot_','',files)
   annotations = gsub('.bed','',annotations)
 
   data = lapply(files, read.table, sep='\t', header=F, quote='', comment.char='', stringsAsFactors=F)
@@ -84,6 +97,6 @@ setwd('~/Desktop')
 
   df = Reduce(rbind, lapply(names(data), function(n){data.frame('annotation'=n, 'avg_diff_meth_in_DM'=data[[n]][,4], stringsAsFactors=F)}))
 
-  avg_diff_meth_in_DM_plot = ggplot(df, aes(avg_diff_meth_in_DM)) + geom_histogram(binwidth=5, aes(y=..density..)) + facet_grid(. ~ annotation) + theme_bw()
+  avg_diff_meth_in_DM_plot = ggplot(df, aes(avg_diff_meth_in_DM)) + geom_histogram(binwidth=5, aes(y=..density..)) + facet_grid(. ~ annotation) + ggtitle('% Methylation Difference Across Annotations') + theme_bw()
 
   ggsave(filename='IDH2mut_v_NBM_mc_hmc_avg_diff_meth_in_DM_annotations.png', plot = avg_diff_meth_in_DM_plot, width = 8, height = 4, dpi = 300)
