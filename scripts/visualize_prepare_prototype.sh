9782f6788b9262f5ad9df8677acca45fc2a74cd4
Raymond Cavalcante <rcavalca@umich.edu>
2015-10-25 10:35:32 -0400

    0.7.5.4 methylSig result visualization
    * Compare methylation difference across all tested sites and all DMRs
    via heatmap of region-matched mu1 and mu0, and histogram of methylation
    differences.
    * Histograms of methylation differences for DMRs across annotations.

--- scripts/visualize_prepare_prototype.sh
+++ scripts/visualize_prepare_prototype.sh
@@ -39,3 +39,22 @@ cd ~/latte/mint/IDH2mut_v_NBM
   bedtools intersect -a ../data/annotation/andersson_permissive_enhancers.bed -b <(zcat ./analysis/bismark_extractor_calls/IDH2mut_1_mc_hmc_trim.fastq.gz_bismark.bismark.cov.gz) -wa -wb | awk -v OFS='\t' '{print $1, $2, $3, $13, $14, $15, $17 + $18}' | bedtools groupby -g 1-3 -c 7 -o mean > IDH2mut_1_mc_hmc_avg_coverage_andersson_permissive_enhancers.bed
 
 ################################################################################
+# Average change in methylation in DMRs in annotated regions
+
+  # For methylSig output
+  # $5 = pvalue, $6 = qvalue, $7 = meth diff, $11 = meth in group 1, $12 = meth in group 0 (methylSig)
+  # $8 = pvalue, $9 = qvalue, $10 = meth diff, $14 = meth in group 1, $15 = meth in group 0 (intersection + CpG island/shore)
+  # $9 = pvalue, $10 = qvalue, $11 = meth diff, $15 = meth in group 1, $16 = meth in group 0 (intersection + promoter)
+  # $17 = pvalue, $18 = qvalue, $19 = meth diff, $23 = meth in group 1, $24 = meth in group 0 (intersection + enhancers)
+
+  # Find average % methylation change in CpG islands
+  bedtools intersect -a ../data/annotation/cpg_islands_hg19_ucsc.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 10 -o mean > IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_cpg_islands.bed
+
+  # Find average % methylation change in CpG islands
+  bedtools intersect -a ../data/annotation/cpg_shores_hg19_ucsc.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 10 -o mean > IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_cpg_shores.bed
+
+  # Find average % methylation change in promoters
+  bedtools intersect -a ../data/annotation/ldef_5kb_hg19_reduced.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-4 -c 11 -o mean | awk -v OFS='\t' '{print $1, $2, $3, $5, $4}' > IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_promoters.bed
+
+  # Find average % methylation change in enhancers
+  bedtools intersect -a ../data/annotation/andersson_permissive_enhancers.bed -b <(awk -v OFS='\t' 'NR > 1 && $5 < 0.05 { print $0 }' ./analysis/methylsig_calls/IDH2mut_v_NBM.txt) -wa -wb | bedtools groupby -g 1-3 -c 19 -o mean > IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_andersson_permissive_enhanceres.bed
--- scripts/visualize_prototype.R
+++ scripts/visualize_prototype.R
@@ -17,7 +17,7 @@ setwd('~/Desktop')
   ggsave(filename='IDH2mut_1_mc_hmc_sample_coverage.png', plot = coverage_plot, width=6, height=6, dpi=300)
 
 ################################################################################
-# Average % methylation across annotations
+# Average % methylation across annotations (bismark.cov.gz)
 
   files = list.files(pattern='avg_meth')
 
@@ -34,7 +34,7 @@ setwd('~/Desktop')
   ggsave(filename='IDH2mut_1_mc_hmc_avg_meth_annotations.png', plot = avg_meth_plot, width = 8, height = 4, dpi = 300)
 
 ################################################################################
-# Average coverage across annotations
+# Average coverage across annotations (bismark.cov.gz)
 
   files = list.files(pattern='avg_coverage')
 
@@ -49,3 +49,41 @@ setwd('~/Desktop')
   avg_meth_plot = ggplot(df, aes(avg_coverage)) + geom_histogram(aes(y=..density..)) + facet_grid(. ~ annotation) + theme_bw()
 
   ggsave(filename='IDH2mut_1_mc_hmc_avg_coverage_annotations.png', plot = avg_meth_plot, width = 8, height = 4, dpi = 300)
+
+################################################################################
+# Average % methylation change across all DMRs (methylSig)
+
+  data = read.table('IDH2mut_v_NBM.txt', header=T, sep='\t', quote='', comment.char='', stringsAsFactors=F)
+  sig = subset(data, pvalue < 0.05)
+
+  avg_DM = data.frame('annotation'='all_tested', 'meth_diff'=data$meth.diff, stringsAsFactors=F)
+  avg_DM_sig = data.frame('annotation'='significantly_DM', 'meth_diff'=sig$meth.diff, stringsAsFactors=F)
+
+  region_DM = data.frame('annotation'='all_tested', 'meth1'=data$mu1, 'meth0'=data$mu0, stringsAsFactors=F)
+  region_DM_sig = data.frame('annotation'='significantly_DM', 'meth1'=sig$mu1, 'meth0'=sig$mu0, stringsAsFactors=F)
+
+  df = rbind(avg_DM, avg_DM_sig)
+  region_df = rbind(region_DM, region_DM_sig)
+
+  diff_meth_plot = ggplot(df, aes(meth_diff)) + geom_histogram(binwidth=5, aes(y=..density..)) + facet_grid(. ~ annotation) + theme_bw()
+  point_plot = ggplot(region_df, aes(meth1, meth0)) + geom_bin2d(binwidth=c(1,1)) + facet_grid(. ~ annotation)
+
+  ggsave(filename='IDH2mut_v_NBM_mc_hmc_diff_meth.png', plot = diff_meth_plot, width = 8, height = 4, dpi = 300)
+  ggsave(filename='IDH2mut_v_NBM_mc_hmc_diff_meth_point_heatmap.png', plot = point_plot, width = 8, height = 4, dpi = 300)
+
+################################################################################
+# Average % methylation change across annotations (methylSig)
+
+  files = list.files(pattern='avg_meth_chg')
+
+  annotations = gsub('IDH2mut_v_NBM_mc_hmc_avg_meth_chg_in_DM_','',files)
+  annotations = gsub('.bed','',annotations)
+
+  data = lapply(files, read.table, sep='\t', header=F, quote='', comment.char='', stringsAsFactors=F)
+  names(data) = annotations
+
+  df = Reduce(rbind, lapply(names(data), function(n){data.frame('annotation'=n, 'avg_diff_meth_in_DM'=data[[n]][,4], stringsAsFactors=F)}))
+
+  avg_diff_meth_in_DM_plot = ggplot(df, aes(avg_diff_meth_in_DM)) + geom_histogram(binwidth=5, aes(y=..density..)) + facet_grid(. ~ annotation) + theme_bw()
+
+  ggsave(filename='IDH2mut_v_NBM_mc_hmc_avg_diff_meth_in_DM_annotations.png', plot = avg_diff_meth_in_DM_plot, width = 8, height = 4, dpi = 300)
