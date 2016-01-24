2924f1fee4d4a45ba26116d9f891d341eedc5d4f
Raymond Cavalcante <rcavalca@umich.edu>
2016-01-24 07:47:00 -0800

    Bugfix: In pulldown comparison, create intermediate file before bigBed

--- scripts/process_pulldown_comparison.sh
+++ scripts/process_pulldown_comparison.sh
@@ -23,6 +23,7 @@ cd ~/latte/mint/${PROJECT}
 # Create appropriate file names
 peprUp=${COMPARISON}__PePr_up_peaks.bed
 peprDown=${COMPARISON}__PePr_down_peaks.bed
+peprCombined=${COMPARISON}_PePr_combined.bed
 peprBigbed=../summary/${PROJECT}_hub/hg19/${COMPARISON}_PePr_peaks.bb
 
 # Since PePr doesn't allow you to specify output directory, cd into the directory
@@ -37,4 +38,5 @@ python2.7 /home/rcavalca/.local/lib/python2.7/site-packages/PePr-1.0.8-py2.7.egg
 
     # This is BED format with the following \t separated columns
     # chrom, start, end, name, score, strand, thickStart, thickEnd, color
-    bedToBigBed <(cat <(awk -v OFS="\t" '{ print $1, $2, $3, "hyper", "1000", ".", $2, $3, "0,0,255" }' $peprUp) <(awk -v OFS="\t" '{ print $1, $2, $3, "hypo", "1000", ".", $2, $3, "102,102,255" }' $peprDown) | sort -T . -k1,1 -k2,2n) ~/latte/Homo_sapiens/chromInfo_hg19.txt $peprBigbed
+    cat <(awk -v OFS="\t" '{ print $1, $2, $3, "hyper", "1000", ".", $2, $3, "0,0,255" }' $peprUp) <(awk -v OFS="\t" '{ print $1, $2, $3, "hypo", "1000", ".", $2, $3, "102,102,255" }' $peprDown) | sort -T . -k1,1 -k2,2n > $peprCombined
+    bedToBigBed $peprCombined ~/latte/Homo_sapiens/chromInfo_hg19.txt $peprBigbed
