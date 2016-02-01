c74fb1e14f61ca663334a4c714cfca009162fe99
Raymond Cavalcante <rcavalca@umich.edu>
2016-01-31 22:41:12 -0800

    Bisulfite alignment vis refactor for annotatr

--- scripts/visualize_align_bisulfite.R
+++ scripts/visualize_align_bisulfite.R
@@ -1,4 +1,4 @@
-library(ggplot2)
+library(annotatr)
 library(optparse)
 
 # Parse arguments
@@ -14,103 +14,90 @@ library(optparse)
 # Set working directory and collect relevant files
   setwd(sprintf('~/latte/mint/%s/', project))
 
-  summary_files = list.files('analysis/summary/tables', full.names=T)
-  summary_files = grep(humanID, summary_files, value=T)
+  summary_tables_dir = './analysis/summary/tables'
 
-  bis_cov = sprintf('analysis/bismark_extractor_calls/%s_trim.fastq.gz_bismark.bismark.cov.gz', humanID)
-
-  files = list(
-    cpg = grep('cpg', summary_files, value=T),
-    windows_5kb = grep('windows', summary_files, value=T),
-    prom_enh = grep('(promoters)|(enhancers)', summary_files, value=T))
+  bis_cov = sprintf('./analysis/bismark_extractor_calls/%s_trimmed.fq.gz_bismark_bt2.bismark.cov.gz', humanID)
+  annot_cov = sprintf('./analysis/bismark_extractor_calls/%s_trimmed.fq.gz_bismark_bt2.CpG_report_for_annotatr.txt.gz', humanID)
 
 ################################################################################
-# % methylation and coverage across all sites in sample
-# Cannot include this in the next block because we get data from one file instead of two
+# % methylation and coverage
 
-  message(sprintf('Plotting percent methylation and coverage across all sites in %s', humanID))
-  data = read.table(bis_cov,
-    header=F, sep='\t', quote='', comment.char='', stringsAsFactors=F,
-    col.names=c('chrom','start','end','perc_meth','numC','numT'))
-  data$coverage = data$numC + data$numT
+  # Read
+  bis_regions = read_bed(
+    annot_cov,
+    col.names = c('chr','start','end','name','coverage','strand','perc_meth'),
+    genome = 'hg19',
+    stranded = TRUE,
+    use.score = TRUE)
 
-  # % methylation
-  png_pm = sprintf('analysis/summary/figures/%s_bisulfite_all_sites_perc_meth.png', humanID)
-  plot_pm =
-    ggplot(data, aes(perc_meth)) +
-    geom_histogram(binwidth=5, aes(y=..density..)) +
-    ggtitle('% Methylation Across All Sites') +
-    theme_bw()
-  ggsave(filename = png_pm, plot = plot_pm, width=6, height=6, dpi=300)
+  # Annotate
+  annotations = c('hg19_basicgenes','hg19_cpgs','hg19_enhancers_fantom')
+  bis_annotated = annotate_regions(
+    regions = bis_regions,
+    annotations = annotations,
+    ignore.strand = TRUE,
+    use.score = TRUE)
 
-  # coverage
-  png_cov = sprintf('analysis/summary/figures/%s_bisulfite_all_sites_cov.png', humanID)
-  plot_cov =
-    ggplot(data, aes(log10(coverage))) +
-    geom_histogram(aes(y=..density..)) +
-    ggtitle('Coverage Across All Sites') +
-    theme_bw()
-  ggsave(filename = png_cov, plot = plot_cov, width=6, height=6, dpi=300)
+  # Setup order vectors
+  all_order = c(
+    'hg19_enhancers_fantom',
+    'hg19_cpg_islands',
+    'hg19_cpg_shores',
+    'hg19_cpg_shelves',
+    'hg19_cpg_inter',
+    'hg19_knownGenes_1to5kb',
+    'hg19_knownGenes_promoters',
+    'hg19_knownGenes_5UTRs',
+    'hg19_knownGenes_exons',
+    'hg19_knownGenes_introns',
+    'hg19_knownGenes_3UTRs')
+  cpgs_order = c(
+    'hg19_cpg_islands',
+    'hg19_cpg_shores',
+    'hg19_cpg_shelves',
+    'hg19_cpg_inter')
+  genes_order = c(
+    'hg19_knownGenes_1to5kb',
+    'hg19_knownGenes_promoters',
+    'hg19_knownGenes_5UTRs',
+    'hg19_knownGenes_exons',
+    'hg19_knownGenes_introns',
+    'hg19_knownGenes_3UTRs')
 
-################################################################################
-# Average % methylation and coverage across CpG annotations and promoter/enhancers
+  # Visualize
+    # Single annotation counts
+    bis_vis_annot_counts = visualize_annotation(
+      annotated_regions = bis_annotated,
+      annotation_order = all_order,
+      plot_title = sprintf('Num. %s CpGs in Annotations', humanID),
+      x_label = 'Annotations',
+      y_label = '# CpGs')
 
-  lapply(names(files), function(annot){
-    message(sprintf('Plotting average percent methylation and average coverage across %s annotations in %s', annot, humanID))
+    # Co-occurring annotation counts
+    bis_vis_coannot_counts = visualize_coannotations(
+      annotated_regions = bis_annotated,
+      annotation_order = all_order,
+      plot_title = sprintf('Num. %s CpGs in Co-occurring Annotations', humanID),
+      axes_label = 'Annotations')
 
-    files_pm = grep('avg_methylation', files[[annot]], value=T)
-    files_cov = grep('avg_coverage', files[[annot]], value=T)
+    # Coverage across Annotations
+    bis_vis_cov = visualize_numerical(
+      tbl = bis_annotated,
+      x = 'coverage',
+      facet = 'annot_type',
+      facet_order = all_order,
+      bin_width = 30,
+      plot_title = sprintf('%s CpG Coverage in Annotations', humanID),
+      x_label = 'Coverage',
+      y_label = 'Density')
 
-    annotations = gsub(sprintf('analysis/summary/tables/%s_avg_methylation_',humanID),'',files_pm)
-    annotations = gsub('.bed','',annotations)
-
-    data_pm = lapply(files_pm, read.table, sep='\t', header=F, quote='', comment.char='', stringsAsFactors=F)
-    names(data_pm) = annotations
-
-    data_cov = lapply(files_cov, read.table, sep='\t', header=F, quote='', comment.char='', stringsAsFactors=F)
-    names(data_cov) = annotations
-
-    df_pm = Reduce(rbind,
-      lapply(names(data_pm), function(n){
-        data.frame(
-          'annotation'=n,
-          'avg_meth'=data_pm[[n]][,5],
-          stringsAsFactors=F)}))
-
-    df_cov = Reduce(rbind,
-      lapply(names(data_cov), function(n){
-        data.frame(
-          'annotation'=n,
-          'avg_coverage'=log10(data_cov[[n]][,5]),
-          stringsAsFactors=F)}))
-
-    if (length(data_pm) == 2) {
-      width = 6
-      height = 4
-    } else if (length(data_pm) > 2) {
-      width = 8
-      height = 4
-    } else {
-      width = 4
-      height = 4
-    }
-
-    png_pm = sprintf('analysis/summary/figures/%s_bisulfite_%s_avg_perc_meth.png', humanID, annot)
-    plot_pm =
-      ggplot(df_pm, aes(avg_meth)) +
-      geom_histogram(binwidth=5, aes(y=..density..)) +
-      facet_grid(. ~ annotation) +
-      ggtitle('Avg. % Meth. Across Annotations') +
-      theme_bw()
-    ggsave(filename = png_pm, plot = plot_pm, width = width, height = height, dpi = 300)
-
-    png_cov = sprintf('analysis/summary/figures/%s_bisulfite_%s_avg_cov.png', humanID, annot)
-    plot_cov =
-      ggplot(df_cov, aes(avg_coverage)) +
-      geom_histogram(aes(y=..density..)) +
-      facet_grid(. ~ annotation) +
-      ggtitle('Avg. Cov. Across Annotations') +
-      theme_bw()
-    ggsave(filename = png_cov, plot = plot_cov, width = width, height = height, dpi = 300)
-
-  })
+    # Coverage across Annotations
+    bis_vis_meth = visualize_numerical(
+      tbl = bis_annotated,
+      x = 'perc_meth',
+      facet = 'annot_type',
+      facet_order = all_order,
+      bin_width = 5,
+      plot_title = sprintf('%s CpG Perc. Meth. in Annotations', humanID),
+      x_label = 'Percent Methylation',
+      y_label = 'Density')
