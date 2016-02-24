78827c7c81b08070cad74c3c1200e3471fe5ca25
Raymond Cavalcante <rcavalca@umich.edu>
2016-02-24 18:34:12 -0500

    Cleanup scripts and instructions

--- init.sh
+++ init.sh
@@ -1,14 +1,16 @@
 PROJECT=$1
 GENOME=$2
+DATAPATH=$3
 
 echo Making directories...
 mkdir projects/${PROJECT}
-mkdir projects/${PROJECT}/data/
+mkdir projects/${PROJECT}/data
 mkdir projects/${PROJECT}/data/raw_fastqs
 mkdir projects/${PROJECT}/{bisulfite,pulldown}
 mkdir projects/${PROJECT}/bisulfite/{raw_fastqs,raw_fastqcs,trim_fastqs,trim_fastqcs,bismark,methylsig_calls}
 mkdir projects/${PROJECT}/pulldown/{raw_fastqs,raw_fastqcs,trim_fastqs,trim_fastqcs,bowtie2_bams,pulldown_coverages,macs_peaks,pepr_peaks}
-mkdir projects/${PROJECT}/{classification_simple,classification_sample,classification_comparison}
+mkdir projects/${PROJECT}/classifications
+mkdir projects/${PROJECT}/classifications/{simple,sample,comparison}
 mkdir projects/${PROJECT}/summary
 mkdir projects/${PROJECT}/summary/{figures,tables,reports}
 mkdir projects/${PROJECT}/${PROJECT}_hub
@@ -24,7 +26,5 @@ cp projects/${PROJECT}_annotation.txt projects/${PROJECT}/data/
 echo Populating file lists...
 awk -f scripts/parse_data.awk projects/${PROJECT}/data/${PROJECT}_annotation.txt > projects/${PROJECT}/variables.mk
 
-# Remove this eventually
-echo Copying data...
-ln -s ~/latte/mint/meta/data/IDH2* ~/latte/mint/projects/${PROJECT}/data/raw_fastqs
-ln -s ~/latte/mint/meta/data/NBM* ~/latte/mint/projects/${PROJECT}/data/raw_fastqs
+echo Symlinking data...
+ln -s ${DATAPATH}/* projects/${PROJECT}/data/raw_fastqs
