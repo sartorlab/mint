fa683bb3a4fd263606fddcb636b8e13fb41cb187
Raymond Cavalcante <rcavalca@umich.edu>
2016-03-06 09:08:43 -0500

    Refactor init.R into modules
    * The script was over 1000 lines and hard to read. Split it into its
    logical parts that are sourced in the right sequence. Easier to
    pinpoint where to fix something.

---
+++ scripts/sub_init_pull_compare.R
@@ -0,0 +1,173 @@
+################################################################################
+# MAKEFILE: pulldown_compare rules
+
+if(bool_pull_comp) {
+	# Keep track of the compares for the master make rules
+	pulldown_compares = c()
+
+	for(i in 1:nrow(pulldown_comparisons)) {
+		# Establish row variables
+		projectID = pulldown_comparisons[i,'projectID']
+		sampleID = pulldown_comparisons[i,'sampleID']
+		humanID = pulldown_comparisons[i,'humanID']
+		pull = pulldown_comparisons[i,'pulldown']
+		bis = pulldown_comparisons[i,'bisulfite']
+		mc = pulldown_comparisons[i,'mc']
+		hmc = pulldown_comparisons[i,'hmc']
+		input = pulldown_comparisons[i,'input']
+		group = pulldown_comparisons[i,'group']
+		fullHumanID = pulldown_comparisons[i,'fullHumanID']
+
+		if( pull == 1 ) {
+		  platform = "pulldown"
+		} else if ( bis == 1 ) {
+		  platform = "bisulfite"
+		}
+
+		if( mc == 1 && hmc == 1 ) {
+		  mark = "mc_hmc"
+		} else if ( mc == 1 && hmc == 0 ) {
+		  mark = "mc"
+		} else if ( mc == 0 && hmc == 1 ) {
+		  mark = "hmc"
+		}
+
+		# Sorting this ensures that the lower group number is A
+		groups = sort(as.integer(unlist(strsplit(group, ','))))
+
+		groupA = subset(pulldown_samples,
+			grepl(groups[1], pulldown_samples$group) &
+			pulldown_samples$pulldown == pull &
+			pulldown_samples$bisulfite == bis &
+			pulldown_samples$mc == mc &
+			pulldown_samples$hmc == hmc &
+			pulldown_samples$input == 0)
+		groupB = subset(pulldown_samples,
+			grepl(groups[2], pulldown_samples$group) &
+			pulldown_samples$pulldown == pull &
+			pulldown_samples$bisulfite == bis &
+			pulldown_samples$mc == mc &
+			pulldown_samples$hmc == hmc &
+			pulldown_samples$input == 0)
+
+		inputGroupA = subset(pulldown_samples,
+			grepl(groups[1], pulldown_samples$group) &
+			pulldown_samples$pulldown == pull &
+			pulldown_samples$bisulfite == bis &
+			pulldown_samples$mc == mc &
+			pulldown_samples$hmc == hmc &
+			pulldown_samples$input == 1)
+		inputGroupB = subset(pulldown_samples,
+			grepl(groups[2], pulldown_samples$group) &
+			pulldown_samples$pulldown == pull &
+			pulldown_samples$bisulfite == bis &
+			pulldown_samples$mc == mc &
+			pulldown_samples$hmc == hmc &
+			pulldown_samples$input == 1)
+
+		########################################################################
+		# Setup variables to put into the makefile
+
+		# For the PePr call from projects/project/pepr_peaks/
+		var_input1 = paste(sprintf('../bowtie2_bams/%s_trimmed.fq.gz_aligned.bam', inputGroupA$fullHumanID), sep='', collapse=',')
+		var_input2 = paste(sprintf('../bowtie2_bams/%s_trimmed.fq.gz_aligned.bam', inputGroupB$fullHumanID), sep='', collapse=',')
+		var_chip1 = paste(sprintf('../bowtie2_bams/%s_trimmed.fq.gz_aligned.bam', groupA$fullHumanID), sep='', collapse=',')
+		var_chip2 = paste(sprintf('../bowtie2_bams/%s_trimmed.fq.gz_aligned.bam', groupB$fullHumanID), sep='', collapse=',')
+
+		# For the prerequisites in the make rule
+		var_merged_input1_pre = paste(sprintf('$(DIR_PULL_COVERAGES)/%s_merged_coverage.bdg', inputGroupA$fullHumanID), sep='', collapse=' ')
+		var_merged_input2_pre = paste(sprintf('$(DIR_PULL_COVERAGES)/%s_merged_coverage.bdg', inputGroupB$fullHumanID), sep='', collapse=' ')
+		var_input1_pre = paste(sprintf('$(DIR_PULL_BOWTIE2)/%s_trimmed.fq.gz_aligned.bam', inputGroupA$fullHumanID), sep='', collapse=' ')
+		var_input2_pre = paste(sprintf('$(DIR_PULL_BOWTIE2)/%s_trimmed.fq.gz_aligned.bam', inputGroupB$fullHumanID), sep='', collapse=' ')
+		var_chip1_pre = paste(sprintf('$(DIR_PULL_BOWTIE2)/%s_trimmed.fq.gz_aligned.bam', groupA$fullHumanID), sep='', collapse=' ')
+		var_chip2_pre = paste(sprintf('$(DIR_PULL_BOWTIE2)/%s_trimmed.fq.gz_aligned.bam', groupB$fullHumanID), sep='', collapse=' ')
+		var_name = fullHumanID
+
+		# Targets
+		input_signal = sprintf('$(DIR_PULL_PEPR)/%s_merged_signal.bed', var_name)
+		up_bed = sprintf('$(DIR_PULL_PEPR)/%s__PePr_up_peaks.bed', var_name)
+		down_bed = sprintf('$(DIR_PULL_PEPR)/%s__PePr_down_peaks.bed', var_name)
+		combined_bed = sprintf('$(DIR_PULL_PEPR)/%s_PePr_combined.bed', var_name)
+		bigbed = sprintf('$(DIR_TRACK)/%s_PePr_peaks.bb', var_name)
+
+		########################################################################
+		# Variables for the makefile
+
+		# Write the pulldown_compare variables for this comparison
+		make_var_pull_compare = c(
+			'################################################################################',
+			'# Workflow for pulldown_compare',
+			sprintf('PULLDOWN_COMPARE_%s_PREREQS := %s %s %s', i, up_bed, bigbed, input_signal),
+			sprintf('PULLDOWN_COMPARE_%s_INPUT1 := %s', i, var_input1),
+			sprintf('PULLDOWN_COMPARE_%s_INPUT2 := %s', i, var_input2),
+			sprintf('PULLDOWN_COMPARE_%s_CHIP1 := %s', i, var_chip1),
+			sprintf('PULLDOWN_COMPARE_%s_CHIP2 := %s', i, var_chip2),
+			sprintf('PULLDOWN_COMPARE_%s_NAME := %s', i, var_name))
+		cat(make_var_pull_compare, file = file_make, sep='\n', append=T)
+
+		# Write the pulldown_compare rule for this comparison
+		make_rule_pull_compare = c(
+			sprintf('.PHONY : pulldown_compare_%s', i),
+			sprintf('pulldown_compare_%s : $(PULLDOWN_COMPARE_%s_PREREQS)', i, i),
+			'',
+			sprintf('%s : %s %s %s %s', up_bed, var_input1_pre, var_input2_pre, var_chip1_pre, var_chip2_pre),
+			'	cd pulldown/pepr_peaks; \\',
+			sprintf('	$(PATH_TO_PEPR) --input1=$(PULLDOWN_COMPARE_%s_INPUT1) --input2=$(PULLDOWN_COMPARE_%s_INPUT2) --chip1=$(PULLDOWN_COMPARE_%s_CHIP1) --chip2=$(PULLDOWN_COMPARE_%s_CHIP2) --name=$(PULLDOWN_COMPARE_%s_NAME) $(OPTS_PEPR)', i, i, i, i, i),
+			sprintf('%s : %s', down_bed, up_bed),
+			'',
+			sprintf('%s : %s %s', combined_bed, up_bed, down_bed),
+			'	bash ../../scripts/combine_pepr.sh $(word 1,$^) $(word 2,$^) $@',
+			'',
+			sprintf('%s : %s %s', input_signal, var_merged_input1_pre, var_merged_input2_pre),
+			'	cat $^ | sort -T . -k1,1 -k2,2n | bedtools merge -d 20 | sort -T . -k1,1 -k2,2n > $@',
+			sprintf('%s : %s', bigbed, combined_bed),
+			'	bedToBigBed $^ $(CHROM_PATH) $@',
+			'')
+		cat(make_rule_pull_compare, file = file_make, sep='\n', append=T)
+
+		# Track the number of pulldown compares
+		pulldown_compares = c(pulldown_compares, sprintf('pulldown_compare_%s', i))
+
+		# trackDb.txt entry for PePr output
+		trackEntry = c(
+		  sprintf('track %s_DM', fullHumanID),
+		  sprintf('parent %s_group_comparison', humanID),
+		  sprintf('bigDataUrl %s_PePr_peaks.bb', fullHumanID),
+		  sprintf('shortLabel %s_DM', fullHumanID),
+		  sprintf('longLabel %s_DM_PePr_peaks', fullHumanID),
+		  'visibility pack',
+		  'itemRgb on',
+		  'type bigBed 9 .',
+		  'priority 1.3',
+		  ' ')
+		cat(trackEntry, file=hubtrackdbfile, sep='\n', append=T)
+	}
+
+	############################################################################
+	# Write the master pulldown_compare rule that will call all created
+	make_rule_master_pull_compare = c(
+		'.PHONY : pulldown_compare',
+		sprintf('pulldown_compare : pulldown_align %s', paste(pulldown_compares, collapse=' ')),
+		'')
+	cat(make_rule_master_pull_compare, file = file_make, sep='\n', append=T)
+
+	#######################################
+	# PBS script
+	pulldown_compare_q = c(
+		'#!/bin/bash',
+		'#### Begin PBS preamble',
+		'#PBS -N pull_compare',
+		'#PBS -l procs=1,mem=32gb,walltime=6:00:00',
+		'#PBS -A sartor_lab',
+		'#PBS -q first',
+		'#PBS -M rcavalca@umich.edu',
+		'#PBS -m abe',
+		'#PBS -j oe',
+		'#PBS -V',
+		'#### End PBS preamble',
+		'# Put your job commands after this line',
+		sprintf('cd ~/latte/mint/projects/%s/',project),
+		'make -j pulldown_compare')
+	cat(pulldown_compare_q, file=sprintf('projects/%s/pulldown_compare.q', project), sep='\n')
+
+}
---
+++ scripts/sub_init_sample_class.R
@@ -0,0 +1,156 @@
+################################################################################
+# MAKEFILE: sample_classification rules
+
+if(bool_bis_samp || bool_pull_samp) {
+
+make_rule_sample_class_bis_module = '
+# Intermediates for the bisulfite piece
+$(DIR_BIS_BISMARK)/%_bisulfite_highmeth.txt $(DIR_BIS_BISMARK)/%_bisulfite_lowmeth.txt $(DIR_BIS_BISMARK)/%_bisulfite_nometh_signal.txt $(DIR_BIS_BISMARK)/%_bisulfite_nometh_nosignal.txt : $(DIR_BIS_BISMARK)/%_bisulfite_trimmed.fq.gz_bismark_bt2.CpG_report.txt
+	awk -f ../../scripts/bisulfite_sample_module.awk $<
+'
+
+make_rule_sample_class_pull_module = '
+# Intermediates for the pulldown piece
+.INTERMEDIATE : $(DIR_PULL_MACS)/%_pulldown_peak.txt
+$(DIR_PULL_MACS)/%_pulldown_peak.txt : $(DIR_PULL_MACS)/%_pulldown_macs2_peaks.narrowPeak
+	awk -v OFS="\\t" \'{print $$1, $$2, $$3}\' $< \\
+	| sort -T . -k1,1 -k2,2n \\
+	> $@
+
+.INTERMEDIATE : $(DIR_PULL_MACS)/%_pulldown_nopeak_signal.txt
+$(DIR_PULL_MACS)/%_pulldown_nopeak_signal.txt : $(DIR_PULL_MACS)/%_pulldown_nopeak.txt $(DIR_PULL_MACS)/%_pulldown_signal.txt
+	bedtools intersect -a $(word 1, $^) -b $(word 2, $^) \\
+	| sort -T . -k1,1 -k2,2n \\
+	> $@
+
+.INTERMEDIATE : $(DIR_PULL_MACS)/%_pulldown_nopeak_nosignal.txt
+$(DIR_PULL_MACS)/%_pulldown_nopeak_nosignal.txt : $(DIR_PULL_MACS)/%_pulldown_nopeak.txt $(DIR_PULL_MACS)/%_pulldown_nosignal.txt
+	bedtools intersect -a $(word 1, $^) -b $(word 2, $^) \\
+	| sort -T . -k1,1 -k2,2n \\
+	> $@
+
+.INTERMEDIATE : $(DIR_PULL_MACS)/%_pulldown_nopeak.txt
+$(DIR_PULL_MACS)/%_pulldown_nopeak.txt : $(DIR_PULL_MACS)/%_pulldown_peak.txt
+	bedtools complement -g <(sort -T . -k1,1 $(CHROM_PATH)) -i $< \\
+	| sort -T . -k1,1 -k2,2n \\
+	> $@
+
+.INTERMEDIATE : $(DIR_PULL_MACS)/%_pulldown_signal.txt
+$(DIR_PULL_MACS)/%_pulldown_signal.txt : $(DIR_PULL_COVERAGES)/%_input_pulldown_merged_coverage.bdg
+	cp $< $@
+
+.INTERMEDIATE : $(DIR_PULL_MACS)/%_pulldown_nosignal.txt
+$(DIR_PULL_MACS)/%_pulldown_nosignal.txt : $(DIR_PULL_MACS)/%_pulldown_signal.txt
+	bedtools complement -g <(sort -T . -k1,1 $(CHROM_PATH)) -i $< \\
+	| sort -T . -k1,1 -k2,2n \\
+	> $@
+'
+
+make_var_sample_class_prefix = sprintf('
+################################################################################
+# Workflow for sample_classification
+SAMPLE_CLASS_PREFIXES := %s', paste(unique(samples$humanID), collapse=' '))
+cat(make_var_sample_class_prefix, file = file_make, sep = '\n', append = TRUE)
+
+# The sample class type depends on the type of samples present
+if(bool_bis_samp && bool_pull_samp) {
+	extra_removes = 'rm -f $(DIR_BIS_BISMARK)/$*_mc_hmc_bisulfite_lowmeth.txt $(DIR_BIS_BISMARK)/$*_mc_hmc_bisulfite_nometh_signal.txt $(DIR_BIS_BISMARK)/$*_mc_hmc_bisulfite_nometh_nosignal.txt $(DIR_PULL_MACS)/$*_hmc_pulldown_peak.txt'
+	sample_class_target = '$(DIR_CLASS_SAMPLE)/%_sample_classification.bed : 	$(DIR_BIS_BISMARK)/%_mc_hmc_bisulfite_highmeth.txt \\
+														$(DIR_BIS_BISMARK)/%_mc_hmc_bisulfite_lowmeth.txt \\
+														$(DIR_BIS_BISMARK)/%_mc_hmc_bisulfite_nometh_signal.txt \\
+														$(DIR_BIS_BISMARK)/%_mc_hmc_bisulfite_nometh_nosignal.txt \\
+														$(DIR_PULL_MACS)/%_hmc_pulldown_peak.txt \\
+														$(DIR_PULL_MACS)/%_hmc_pulldown_nopeak_signal.txt \\
+														$(DIR_PULL_MACS)/%_hmc_pulldown_nopeak_nosignal.txt'
+	rule1 = make_rule_sample_class_bis_module
+	rule2 = make_rule_sample_class_pull_module
+	class_script = '../../scripts/classify_hybrid_sample.sh'
+} else if (bool_bis_samp && !bool_pull_samp) {
+	############################################################
+	# NOTE: THIS IS NOT EXPLICITLY SUPPORTED RIGHT NOW
+	############################################################
+	extra_removes = 'rm -f $(DIR_BIS_BISMARK)/$*_mc_bisulfite_lowmeth.txt $(DIR_BIS_BISMARK)/$*_mc_bisulfite_nometh_signal.txt $(DIR_BIS_BISMARK)/$*_mc_bisulfite_nometh_nosignal.txt $(DIR_BIS_BISMARK)/$*_hmc_bisulfite_lowmeth.txt $(DIR_BIS_BISMARK)/$*_hmc_bisulfite_nometh_signal.txt $(DIR_BIS_BISMARK)/$*_hmc_bisulfite_nometh_nosignal.txt'
+	sample_class_target = '$(DIR_CLASS_SAMPLE)/%_sample_classification.bed : 	$(DIR_BIS_BISMARK)/%_mc_bisulfite_highmeth.txt \\
+														$(DIR_BIS_BISMARK)/%_mc_bisulfite_lowmeth.txt \\
+														$(DIR_BIS_BISMARK)/%_mc_bisulfite_nometh_signal.txt \\
+														$(DIR_BIS_BISMARK)/%_mc_bisulfite_nometh_nosignal.txt \\
+														$(DIR_BIS_BISMARK)/%_hmc_bisulfite_highmeth.txt \\
+														$(DIR_BIS_BISMARK)/%_hmc_bisulfite_lowmeth.txt \\
+														$(DIR_BIS_BISMARK)/%_hmc_bisulfite_nometh_signal.txt \\
+														$(DIR_BIS_BISMARK)/%_hmc_bisulfite_nometh_nosignal.txt'
+	rule1 = make_rule_sample_class_bis_module
+	rule2 = ''
+	class_script = '../../scripts/classify_bisulfite_sample.sh'
+} else {
+	extra_removes = 'rm -f $(DIR_PULL_MACS)/$*_mc_pulldown_peak.txt $(DIR_PULL_MACS)/$*_hmc_pulldown_peak.txt'
+	sample_class_target = '$(DIR_CLASS_SAMPLE)/%_sample_classification.bed : 	$(DIR_PULL_MACS)/%_mc_pulldown_peak.txt \\
+														$(DIR_PULL_MACS)/%_mc_pulldown_nopeak_signal.txt \\
+														$(DIR_PULL_MACS)/%_mc_pulldown_nopeak_nosignal.txt \\
+														$(DIR_PULL_MACS)/%_hmc_pulldown_peak.txt \\
+														$(DIR_PULL_MACS)/%_hmc_pulldown_nopeak_signal.txt \\
+														$(DIR_PULL_MACS)/%_hmc_pulldown_nopeak_nosignal.txt'
+	rule1 = make_rule_sample_class_pull_module
+	rule2 = ''
+	class_script = '../../scripts/classify_pulldown_sample.sh'
+}
+
+make_rule_class_sample = sprintf('
+# Master rule
+.PHONY : sample_classification
+sample_classification : 	$(patsubst %%,$(DIR_TRACK)/%%_sample_classification.bb,$(SAMPLE_CLASS_PREFIXES)) \\
+		$(patsubst %%,$(DIR_CLASS_SAMPLE)/%%_sample_classification.bed,$(SAMPLE_CLASS_PREFIXES))
+
+# Rule for sample classification bigBed
+$(DIR_TRACK)/%%_sample_classification.bb : $(DIR_CLASS_SAMPLE)/%%_sample_classification.bed
+	bedToBigBed $^ $(CHROM_PATH) $@
+
+# NOTE: There is a known bug in make that incorrectly determines implicit intermediate
+# files when they occur in a list of multiple targets and prerequisites.
+# https://savannah.gnu.org/bugs/index.php?32042
+# The easiest workaround is to remove the ones make does not automatically remove
+
+# Classification BED
+%s
+	bash %s $(CHROM_PATH) $@ $^
+
+%s
+%s',
+	sample_class_target, class_script, rule1, rule2)
+cat(make_rule_class_sample, file = file_make, sep = '\n', append = TRUE)
+
+#######################################
+# PBS script
+pulldown_sample_q = c(
+	'#!/bin/bash',
+	'#### Begin PBS preamble',
+	'#PBS -N class_sample',
+	'#PBS -l procs=4,mem=48gb,walltime=6:00:00',
+	'#PBS -A sartor_lab',
+	'#PBS -q first',
+	'#PBS -M rcavalca@umich.edu',
+	'#PBS -m abe',
+	'#PBS -j oe',
+	'#PBS -V',
+	'#### End PBS preamble',
+	'# Put your job commands after this line',
+	sprintf('cd ~/latte/mint/projects/%s/',project),
+	'make -j 4 sample_classification')
+cat(pulldown_sample_q, file=sprintf('projects/%s/classify_sample.q', project), sep='\n')
+
+for(sample in unique(samples$humanID)) {
+	# trackDb.txt entry for sample classification
+	trackEntry = c(
+	  sprintf('track %s_sample_classification', sample),
+	  sprintf('parent %s_sample', sample),
+	  sprintf('bigDataUrl %s_sample_classification.bb', sample),
+	  sprintf('shortLabel %s_sample_class', sample),
+	  sprintf('longLabel %s_sample_classification', sample),
+	  'visibility pack',
+	  'itemRgb on',
+	  'type bigBed 9 .',
+	  'priority 1.1',
+	  ' ')
+	cat(trackEntry, file=hubtrackdbfile, sep='\n', append=T)
+}
+
+}
