049de15550b77f31c2310359e6e89fd6a2681615
Raymond Cavalcante <rcavalca@umich.edu>
2016-04-01 15:04:44 -0400

    Generalize template_config.mk for release
    * Remove references to latte which are specific to our lab.
    * Remove particular path to PePr as well.

--- template_config.mk
+++ template_config.mk
@@ -1,9 +1,13 @@
 
 ################################################################################
 # Genome paths
-GENOME_PATH := ~/latte/Homo_sapiens/
-BOWTIE2_GENOME_PATH := ~/latte/Homo_sapiens/genome
-CHROM_PATH := ~/latte/Homo_sapiens/chromInfo_hg19.txt
+# Genomes, like as downloaded from iGenomes, will also contain bisulfite
+# converted genomes for
+GENOME_PATH := /path/to/genome
+# Location of bowtie2 indexes
+BOWTIE2_GENOME_PATH := /path/to/bowtie2/indices
+# Location of chromosome length file
+CHROM_PATH := /path/to/chromosome/length/file
 
 ################################################################################
 # Path to tools
@@ -15,7 +19,7 @@ PATH_TO_EXTRACTOR := $(shell which bismark_methylation_extractor)
 PATH_TO_BOWTIE2 := $(shell which bowtie2)
 PATH_TO_R := $(shell which Rscript)
 PATH_TO_MACS := $(shell which macs2)
-PATH_TO_PEPR := python2.7 /home/rcavalca/.local/lib/python2.7/site-packages/PePr-1.0.8-py2.7.egg/PePr/PePr.py
+PATH_TO_PEPR := $(shell which PePr)
 PATH_TO_SAMTOOLS := $(shell which samtools)
 PATH_TO_BEDTOOLS := $(shell which bedtools)
 PATH_TO_BEDOPS := $(shell which bedops)
