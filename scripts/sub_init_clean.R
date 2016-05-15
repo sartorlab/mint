1650adb3d6efb6cc472084e588eddb4dad0f9f4c
Raymond Cavalcante <rcavalca@umich.edu>
2016-05-15 10:32:45 -0400

    Bugfix: Depending on experiment, use certain clean rules

--- scripts/sub_init_clean.R
+++ scripts/sub_init_clean.R
@@ -1,4 +1,24 @@
-make_clean_tmp_rule = '.PHONY : clean_tmp
-clean_tmp : clean_bisulfite_align_tmp clean_bisulfite_compare_tmp clean_pulldown_sample_tmp clean_pulldown_compare_tmp clean_sample_classification_tmp clean_compare_classification_tmp'
+################################################################################
+# MAKEFILE: clean rules
+
+clean_str = ''
+if(bool_bis_samp) {
+	clean_str = paste(clean_str, 'clean_bisulfite_align_tmp')
+}
+
+if(bool_pull_samp) {
+	clean_str = paste(clean_str, 'clean_pulldown_sample_tmp', 'clean_sample_classification_tmp')
+}
+
+if(bool_bis_comp) {
+	clean_str = paste(clean_str, 'clean_bisulfite_compare_tmp', 'clean_compare_classification_tmp')
+}
+
+if(bool_pull_comp) {
+	clean_str = paste(clean_str, 'clean_pulldown_compare_tmp')
+}
+
+make_clean_tmp_rule = sprintf('.PHONY : clean_tmp
+clean_tmp : %s', clean_str)
 
 cat(make_clean_tmp_rule, file = file_make, sep = '\n', append = TRUE)
