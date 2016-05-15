5e053037a7df7270318c4123332c8a81c1bd9078
Raymond Cavalcante <rcavalca@umich.edu>
2016-05-15 10:19:40 -0400

    Bugfix: clean_tmp needs prerequisites
    * Put what should have been prerequisites in the wrong part of the rule

--- scripts/sub_init_clean.R
+++ scripts/sub_init_clean.R
@@ -1,5 +1,4 @@
 make_clean_tmp_rule = '.PHONY : clean_tmp
-clean_tmp :
-	clean_bisulfite_align_tmp clean_bisulfite_compare_tmp clean_pulldown_sample_tmp clean_pulldown_compare_tmp clean_sample_classification_tmp clean_compare_classification_tmp'
+clean_tmp : clean_bisulfite_align_tmp clean_bisulfite_compare_tmp clean_pulldown_sample_tmp clean_pulldown_compare_tmp clean_sample_classification_tmp clean_compare_classification_tmp'
 
 cat(make_clean_tmp_rule, file = file_make, sep = '\n', append = TRUE)
