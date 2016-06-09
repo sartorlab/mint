d72126fe61fd10d10fe45da00b02cfe1bc3b0bfc
Raymond Cavalcante <rcavalca@umich.edu>
2016-06-09 13:56:50 -0400

    Merge adjacent sample classifications
    * Similar solution, but need to pay attention to different group
    numbers from the particular situations.

--- scripts/classify_hybrid_sample.sh
+++ scripts/classify_hybrid_sample.sh
@@ -22,6 +22,15 @@ intTmp=classifications/sample/${ID}_tmpIntersect.txt
 classTmp=classifications/sample/${ID}_tmpSampleClass.txt
 joinTmp=classifications/sample/${ID}_tmpSampleJoin.txt
 
+# tmp class files
+tmp6101422=classifications/comparison/${ID}_tmp6101422.txt
+tmp12=classifications/comparison/${ID}_tmp12.txt
+tmp20=classifications/comparison/${ID}_tmp20.txt
+tmp18=classifications/comparison/${ID}_tmp18.txt
+tmp30=classifications/comparison/${ID}_tmp30.txt
+tmp284244=classifications/comparison/${ID}_tmp284244.txt
+tmp66=classifications/comparison/${ID}_tmp66.txt
+
 # Initial intersection
 ${PATH_TO_BEDTOOLS} multiinter \
 	-header \
@@ -34,10 +43,43 @@ ${PATH_TO_BEDTOOLS} multiinter \
 # Encode each region with the appropriate classification
 # Columns 6-12 are the binary columns that should be operated on
 ${PATH_TO_AWK} -v OFS="\t" 'NR > 1 { \
+	outFile = FILENAME; \
 	group1 = ($6 * 3) + ($7 * 5) + ($8 * 7) + ($9 * 11); \
 	group2 = ($10 * 2) + ($11 * 4) + ($12 * 6); \
-	if (group1 * group2 > 0) print $1, $2, $3, group1, group2, group1 * group2; \
-}' ${intTmp} > ${classTmp}
+	if (group1 * group2 == 6 || group1 * group2 == 10 || group1 * group2 == 14 || group1 * group2 == 22) { \
+		sub(/tmpIntersect/, "tmp6101422", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 12) { \
+		sub(/tmpIntersect/, "tmp12", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 20) { \
+		sub(/tmpIntersect/, "tmp20", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 18) { \
+		sub(/tmpIntersect/, "tmp18", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 30) { \
+		sub(/tmpIntersect/, "tmp30", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 28 || group1 * group2 == 42 || group1 * group2 == 44) { \
+		sub(/tmpIntersect/, "tmp284244", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 66) { \
+		sub(/tmpIntersect/, "tmp66", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} \
+}' ${intTmp}
+
+# Bedtools merge each of the files
+tmpClassFiles=( ${tmp6101422} ${tmp12} ${tmp20} ${tmp18} ${tmp30} ${tmp284244} ${tmp66} )
+for file in "${tmpClassFiles[@]}"
+do
+	bedtools merge -i <(sort -T . -k1,1 -k2,2n ${file}) -c 4,5,6 -o first,first,first > ${file}.merged
+done
+
+# Combine and sort in preparation for joining with the table
+cat ${tmp6101422}.merged ${tmp12}.merged ${tmp20}.merged ${tmp18}.merged ${tmp30}.merged ${tmp284244}.merged ${tmp66}.merged | sort -T . -k1,1 -k2,2n > ${classTmp}
+
 
 # Join on the classification code
 # NOTE: Files must be sorted on the join field (code)
@@ -54,4 +96,4 @@ ${PATH_TO_AWK} -v OFS="\t" '{
 }' ${joinTmp} | sort -T . -k1,1 -k2,2n > ${outFile}
 
 # Cleanup
-rm -f ${intTmp} ${classTmp} ${joinTmp}
+rm -f ${intTmp} ${classTmp} ${joinTmp} ${tmp6101422} ${tmp12} ${tmp20} ${tmp18} ${tmp30} ${tmp284244} ${tmp66} ${tmp6101422}.merged ${tmp12}.merged ${tmp20}.merged ${tmp18}.merged ${tmp30}.merged ${tmp284244}.merged ${tmp66}.merged
--- scripts/classify_pulldown_sample.sh
+++ scripts/classify_pulldown_sample.sh
@@ -21,6 +21,13 @@ intTmp=classifications/sample/${ID}_tmpIntersect.txt
 classTmp=classifications/sample/${ID}_tmpSampleClass.txt
 joinTmp=classifications/sample/${ID}_tmpSampleJoin.txt
 
+# tmp class files
+tmp6=classifications/comparison/${ID}_tmp6.txt
+tmp1014=classifications/comparison/${ID}_tmp1014.txt
+tmp1218=classifications/comparison/${ID}_tmp1218.txt
+tmp202830=classifications/comparison/${ID}_tmp202830.txt
+tmp42=classifications/comparison/${ID}_tmp42.txt
+
 # Initial intersection
 ${PATH_TO_BEDTOOLS} multiinter \
 	-header \
@@ -33,10 +40,36 @@ ${PATH_TO_BEDTOOLS} multiinter \
 # Encode each region with the appropriate classification
 # Columns 6-11 are the binary columns that should be operated on
 ${PATH_TO_AWK} -v OFS="\t" 'NR > 1 { \
+	outFile = FILENAME; \
 	group1 = ($6 * 3) + ($7 * 5) + ($8 * 7); \
 	group2 = ($9 * 2) + ($10 * 4) + ($11 * 6); \
-	if (group1 * group2 > 0) print $1, $2, $3, group1, group2, group1 * group2; \
-}' ${intTmp} > ${classTmp}
+	if (group1 * group2 == 6) { \
+		sub(/tmpIntersect/, "tmp6", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 10 || group1 * group2 == 14) { \
+		sub(/tmpIntersect/, "tmp1014", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 12 || group1 * group2 == 18) { \
+		sub(/tmpIntersect/, "tmp1218", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 20 || group1 * group2 == 28 || group1 * group2 == 30) { \
+		sub(/tmpIntersect/, "tmp202830", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} else if (group1 * group2 == 42) { \
+		sub(/tmpIntersect/, "tmp42", outFile); \
+		print $1, $2, $3, group1, group2, group1 * group2 > outFile; \
+	} \
+}' ${intTmp}
+
+# Bedtools merge each of the files
+tmpClassFiles=( ${tmp6} ${tmp1014} ${tmp1218} ${tmp202830} ${tmp42} )
+for file in "${tmpClassFiles[@]}"
+do
+	bedtools merge -i <(sort -T . -k1,1 -k2,2n ${file}) -c 4,5,6 -o first,first,first > ${file}.merged
+done
+
+# Combine and sort in preparation for joining with the table
+cat ${tmp6}.merged ${tmp1014}.merged ${tmp1218}.merged ${tmp202830}.merged ${tmp42}.merged | sort -T . -k1,1 -k2,2n > ${classTmp}
 
 # Join on the classification code
 # NOTE: Files must be sorted on the join field (code)
@@ -53,4 +86,4 @@ ${PATH_TO_AWK} -v OFS="\t" '{
 }' ${joinTmp} | sort -T . -k1,1 -k2,2n > ${outFile}
 
 # Cleanup
-rm -f ${intTmp} ${classTmp} ${joinTmp}
+rm -f ${intTmp} ${classTmp} ${joinTmp} ${tmp6} ${tmp1014} ${tmp1218} ${tmp202830} ${tmp42} ${tmp6}.merged ${tmp1014}.merged ${tmp1218}.merged ${tmp202830}.merged ${tmp42}.merged
