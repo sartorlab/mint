1ae2a8b2145e036c794006d00433dd1863e24946
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2016-07-13 10:44:12 -0400

    Refactor annotatr module of mint
    * The initial commit of this file in ba7b1fb was a mistake.
    * Added clarification of simple classifications for macs2, PePr, and
    bismark.
    * May need to alter some prefixes in future.

--- scripts/annotatr_annotations.R
+++ scripts/annotatr_annotations.R
@@ -10,7 +10,7 @@ library(optparse)
 option_list = list(
   make_option('--file', type='character', help='[Required] Tab-delimited file with genomic locations and possibly associated data.'),
   make_option('--genome', type='character', help='[Required] The shortname for the genome used, e.g. hg19, mm9, rn4.'),
-  make_option('--annot_type', type='character', help='[Required] One of bismark, simple_bisulfite, macs2, simple_pulldown, sample_class, methylSig, PePr, or compare_class. Indicates what type of data is being annotated.'),
+  make_option('--annot_type', type='character', help='[Required] One of bismark, simple_bisulfite_bismark, macs2, simple_pulldown_macs2, sample_class, methylSig, PePr, simple_pulldown_PePr, or compare_class. Indicates what type of data is being annotated.'),
   make_option('--group1', type='character', help='[Required] A character indicating the name of group1 or NULL.'),
   make_option('--group0', type='character', help='[Required] A character indicating the name of group2 or NULL.')
 )
@@ -38,7 +38,7 @@ if(annot_type == 'bismark') {
 	col_types = 'ci-di-'
 	stranded = FALSE
 	prefix = gsub('_bt2.bismark.cov.gz','', basename(file))
-} else if (annot_type == 'simple_bisulfite') {
+} else if (annot_type == 'simple_bisulfite_bismark') {
 	# head ./classifications/simple/IDH2mut_1_mc_hmc_bisulfite_simple_classification.bed
 	# chr1	249239097	249239098	mc_hmc_high	1000	.	249239097	249239098	102,0,102
 	# chr1	249239100	249239101	mc_hmc_high	1000	.	249239100	249239101	102,0,102
@@ -47,7 +47,7 @@ if(annot_type == 'bismark') {
 	col_names = c('chr','start','end','class')
 	col_types = 'ciic-----'
 	stranded = FALSE
-	prefix =
+	prefix = gsub('.bed','', basename(file))
 } else if (annot_type == 'macs2') {
 	# head ./pulldown/macs2_peaks/IDH2mut_2_hmc_pulldown_macs2_peaks.narrowPeak
 	# chr21	9909565	9910066	IDH2mut_2_hmc_pulldown_macs2_peak_1	126	.	3.04226	16.46495	12.64028	213
@@ -58,7 +58,7 @@ if(annot_type == 'bismark') {
 	col_types = 'ciic--dd--'
 	stranded = FALSE
 	prefix = gsub('_peaks.narrowPeak','', basename(file))
-} else if (annot_type == 'simple_pulldown') {
+} else if (annot_type == 'simple_pulldown_macs2') {
 	# head ./classifications/simple/IDH2mut_1_hmc_pulldown_simple_classification.bed
 	# chr21	9944160	9944341	hmc_low	1000	.	9944160	9944341	102,102,255
 	# chr21	9999539	9999701	hmc_low	1000	.	9999539	9999701	102,102,255
@@ -67,7 +67,7 @@ if(annot_type == 'bismark') {
 	col_names = c('chr','start','end','class')
 	col_types = 'ciic----'
 	stranded = FALSE
-	prefix =
+	prefix = gsub('.bed','', basename(file))
 } else if (annot_type == 'sample_class') {
 	# head ./classifications/sample/NBM_1_sample_classification.bed
 	# chr1	10468	10472	unclassifiable	1000	.	10468	10472	192,192,192
@@ -80,7 +80,6 @@ if(annot_type == 'bismark') {
 	prefix = gsub('.bed','', basename(file))
 } else if (annot_type == 'methylSig') {
 	# head ./bisulfite/methylsig_calls/IDH2mut_v_NBM_mc_hmc_bisulfite_DMR_methylSig.txt
-	# chr	start	end	strand	pvalue	qvalue	meth.diff	logLikRatio	theta	df	mu1	mu0
 	# chr21	9437472	9437521	*	0.000228579898882106	0.0519394743163992	28.6754246208103	158.6873713159	1e+06	4	81.4241341438466	52.7487095230364
 	# chr21	9437522	9437571	*	0.0208553163921381	0.283830375900465	22.738738618692	13.6819351620397	49.7403416134298	4	85.8751984050635	63.1364597863715
 	# chr21	9438322	9438371	*	0.777292550560113	0.922829646963547	-2.62511686393549	0.0915541817157646	23.6729453104579	4	75.0320958189817	77.6572126829172
@@ -97,7 +96,17 @@ if(annot_type == 'bismark') {
 	col_names = c('chr','start','end','name','fold','pval')
 	col_types = 'ciic--dd-'
 	stranded = FALSE
-	prefix =
+	prefix = gsub('.bed','', basename(file))
+} else if (annot_type == 'simple_pulldown_PePr') {
+	# head ./classifications/simple/IDH2mut_1_hmc_pulldown_simple_classification.bed
+	# chr21	9944160	9944341	diff_hmc_weak	1000	.	9944160	9944341	102,102,255
+	# chr21	9999539	9999701	diff_hmc_weak	1000	.	9999539	9999701	102,102,255
+	# chr21	10132638	10133209	diff_hmc_weak	1000	.	10132638	10133209	102,102,255
+	# chr21	10135234	10135504	diff_hmc_weak	1000	.	10135234	10135504	102,102,255
+	col_names = c('chr','start','end','class')
+	col_types = 'ciic----'
+	stranded = FALSE
+	prefix = gsub('.bed','', basename(file))
 } else if (annot_type == 'compare_class') {
 	# head ./classifications/comparison/IDH2mut_v_NBM_compare_classification.bed
 	# chr1	0	10000	unclassifiable	1000	.	0	10000	192,192,192
