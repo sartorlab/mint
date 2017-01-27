4c401e36305b0012ed9e1c12384766224da89000
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-01-27 13:44:29 -0500

    Minimize R packages in script
    * All dependencies should be installed, even from Bioconductor, automatically.

--- install/r_packages.R
+++ install/r_packages.R
@@ -1,6 +1,4 @@
-install.packages(c('devtools','optparse','readr','dplyr','ggplot2'), repos='http://cran.rstudio.com')
-source('http://bioconductor.org/biocLite.R')
-biocLite(c('GenomeInfoDb','GenomicRanges','IRanges','regioneR'))
+install.packages(c('devtools','optparse'), repos='http://cran.rstudio.com')
 
 devtools::install_github('rcavalcante/annotatr@v1.1.7')
 devtools::install_github('sartorlab/methylSig@v0.4.4')
