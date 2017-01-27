40edc97c0f85773f84410b1fefa952c584253cc2
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2017-01-27 13:14:43 -0500

    Need paths relative to project roots for apps
    * And bismark_methylation_extractor needs to be different because of the way we cd into bisulfite/bismark to run it.

--- template_config.mk
+++ template_config.mk
@@ -3,18 +3,18 @@
 # Path to tools
 
 PATH_TO_AWK := $(shell which awk)
-PATH_TO_BDG2BW := apps/bin/bedGraphToBigWig
-PATH_TO_BED2BB := apps/bin/bedToBigBed
-PATH_TO_BEDOPS := apps/bin/bedops
-PATH_TO_BEDTOOLS := apps/bedtools2/bin/bedtools
-PATH_TO_BISMARK := apps/bismark/bismark
-PATH_TO_BOWTIE2 := apps/bowtie2/bowtie2
+PATH_TO_BDG2BW := ../../apps/bin/bedGraphToBigWig
+PATH_TO_BED2BB := ../../apps/bin/bedToBigBed
+PATH_TO_BEDOPS := ../../apps/bin/bedops
+PATH_TO_BEDTOOLS := ../../apps/bedtools2/bin/bedtools
+PATH_TO_BISMARK := ../../apps/bismark/bismark
+PATH_TO_BOWTIE2 := ../../apps/bowtie2/bowtie2
 PATH_TO_CUTADAPT := $(shell which cutadapt)
-PATH_TO_EXTRACTOR := apps/bismark/bismark_methylation_extractor
-PATH_TO_FASTQC := apps/FastQC/fastqc
+PATH_TO_EXTRACTOR := ../../../../apps/bismark/bismark_methylation_extractor
+PATH_TO_FASTQC := ../../apps/FastQC/fastqc
 PATH_TO_MACS := $(shell which macs2)
 PATH_TO_MULTIQC := $(shell which multiqc)
 PATH_TO_PEPR := $(shell which pepr)
 PATH_TO_R := $(shell which Rscript)
-PATH_TO_SAMTOOLS := apps/samtools/bin/samtools
-PATH_TO_TRIMGALORE := apps/trim_galore/trim_galore
+PATH_TO_SAMTOOLS := ../../apps/samtools/bin/samtools
+PATH_TO_TRIMGALORE := ../../apps/trim_galore/trim_galore
